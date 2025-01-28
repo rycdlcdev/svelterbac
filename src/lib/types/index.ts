6cdb8f0cb8c1190de5ae5c6f3a275f21abc09449
David <rycdlcdev@gmail.com>
2025-01-28 15:18:37 -0600

    Añadir archivo de tipos: importar y re-exportar tipos de respuesta de API y solicitudes de autenticación

---
+++ src/lib/types/index.ts
@@ -0,0 +1,6 @@
+// import and re-export all types from the other files
+
+import type { APIResponse } from './apiResponse';
+import type { LoginRequest, RegisterRequest } from './auth';
+
+export type { APIResponse, LoginRequest, RegisterRequest };
